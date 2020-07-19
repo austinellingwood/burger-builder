db42a9b5567c6288e98c98d9764d90cccafc21ce
Austin Ellingwood <austin@viralspace.ai>
2020-07-18 17:57:28 -0700

    section 9 and 10, adding in axios, http requests to a firebase backend, get ingredients from db and posting orders

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Spinner.module.css';
+
+const spinner = () => (
+        <div className={styles.Spinner}>Sending Order...</div>
+);
+
+export default spinner;
